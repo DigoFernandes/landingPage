d986c410063f90aa62a85da6bfcbb8a22ec58e6b
DigoFernandes <digofernandes017@gmail.com>
2024-03-05 00:19:25 -0300

    Section service 90%

--- src/components/SectionServices.tsx
+++ src/components/SectionServices.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Container from "./Container";
+import IconPhone from "@/assets/icon-phone.svg"
+import Image from "next/image";
 
 export default function SectionServices() {
   return (
@@ -12,18 +14,30 @@ export default function SectionServices() {
           <h2 className="text-primary-gray text-[56px] font-bold leading-tight mb-6">
             Gerencie suas finanças sem sair de casa
           </h2>
-          <p className="text-lg max-w-[554px] mb-15 text-second-gray">
+          <p className="text-lg max-w-[554px] mb-10 text-second-gray">
             Veja como você pode cuidar das suas finanças pelo app Itaú de forma
             segura,rápida e o melhor, no conforto de casa
           </p>
 
           <ul>
-            <li>
-                <p>Acompanhar sua conta, fazer transferencias e pagamentos onde estiver</p>
+            <li className="flex items-center gap-9 pb-9 border-b-[1px] border-opacity-gray">
+              <div className="w-7 h-7 flex items-center justify-center">
+                <Image src={IconPhone} alt="Phone icon" />
+              </div>
+              <p className="flex-1 text-txt-gray pr-2">
+                Acompanhar sua conta, fazer transferencias e pagamentos onde
+                estiver
+              </p>
             </li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li className="flex items-center gap-9 pb-9 border-b-[1px] border-opacity-gray">
+              <div className="w-7 h-7 flex items-center justify-center">
+                <Image src={IconPhone} alt="Phone icon" />
+              </div>
+              <p className="flex-1 text-txt-gray pr-2">
+               Soluções de empresas e renegociação para organizar suas finanças
+              </p>
+            </li>
+            
           </ul>
         </div>
       </Container>
